51a9df40e598a7fd5ea1f03f97088aaeb2403dc4
N0TRIA <airtonlameraaliivl@gmail.com>
2022-11-10 15:53:02 -0300

    Trabalhando com datas
    - Utilizar componente de calendário
    - Manipular datas
    - Desestruturar objetos em JavaScript

--- projeto_inicial/main.js
+++ projeto_inicial/main.js
@@ -1,27 +1,42 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
- 
-    const criarTarefa = (evento) => {
 
+const handleNovoItem = (evento) => {
     evento.preventDefault()
-
+    
     const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
+    
+    const calendario = document.querySelector('[data-form-date]');
+    const data = moment(calendario.value);
+    const dataFormatada = data.format('DD/MM/YYYY');
 
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+    const dados = {
+        valor,
+        dataFormatada
+    }
 
-    tarefa.innerHTML = conteudo
+    const criaTarefa = criarTarefa(dados);
 
-    tarefa.appendChild(BotaoConclui())
-    tarefa.appendChild(BotaoDeleta())
-    lista.appendChild(tarefa)
-    input.value = " "
+    lista.appendChild(criaTarefa)
+    input.value = " ";
+}
 
+const criarTarefa = ({valor, dataFormatada}) => {
+
+const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+
+    tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
+    
+    return tarefa;
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
